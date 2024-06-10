2a7f659f6b062a53c952eb41a6fc96a259a86857
mir312 <dakievameerim0@gmail.com>
2024-06-10 17:45:20 +0600

    add text for video

--- src/Pages/Women/Women.js
+++ src/Pages/Women/Women.js
@@ -4,10 +4,29 @@ import video_women from "../../video/adi_women.mp4"
 function Women() {
   return (
   
+    <section className='women'>
     <video className='men_video'  autoPlay muted>
     <source src={video_women} type="video/mp4"></source>
-  </video>
+</video>
 
+<div className="container">
+<div className="women_block">
+          
+
+          <p className="kids_desc"> Engineered for speed, the adidas F50 is here to free the last </p>
+
+          <button className="btn_kids flex">SHOP NOW
+            <i className="icon_kids"></i>
+          </button>
+
+        </div>
+
+
+        
+</div>
+
+      
+  </section>
   );
 }
 
